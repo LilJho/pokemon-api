0740c821b2a698e964c79ebe293ea8e54f44a17f
LilJho <jhonnelg25@gmail.com>
2023-06-09 20:15:45 +0800

    refactored pokemonCard component and Pokemon Page

--- src/components/screen/PokemonCard.tsx
+++ src/components/screen/PokemonCard.tsx
@@ -1,41 +1,16 @@
 import React, { useEffect, useState } from "react";
 
 interface PokemonCardProps {
-  pokemonUrl: string;
+  pokemonName: string;
+  pokemonImg: string;
 }
 
-interface PokemonTypes {
-  sprite: {
-    front_default: string;
-  };
-  name: string;
-}
-
-const PokemonCard = ({ pokemonUrl }: PokemonCardProps) => {
-  const [pokemon, setPokemon] = useState<PokemonTypes>();
-  const [pokemonImg, setPokemonImg] = useState<string>();
-
-  useEffect(() => {
-    const getPokemonData = async () => {
-      const response = await fetch(pokemonUrl);
-      const data = await response.json();
-      console.log(data);
-
-      setPokemon(data);
-      setPokemonImg(data.sprites.front_default);
-    };
-    getPokemonData();
-  }, [pokemonUrl]);
-
-  if (pokemon === undefined) {
-    return <span>Loading</span>;
-  }
-
+const PokemonCard = ({ pokemonName, pokemonImg }: PokemonCardProps) => {
   return (
     <div className="flex items-center justify-center w-screen h-screen">
       <div className="p-4 bg-gray-800 rounded-lg">
-        <img src={pokemonImg} alt={pokemon.name} />
-        <p className="text-white">{pokemon.name}</p>
+        <img src={pokemonImg} alt={pokemonName} />
+        <p className="text-white">{pokemonName}</p>
       </div>
     </div>
   );
--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import PokemonCard from "../components/screen/PokemonCard";
 
@@ -6,7 +6,21 @@ const Pokemon = () => {
   const location = useLocation();
   const { pokemonUrl } = location.state;
 
-  return <PokemonCard pokemonUrl={pokemonUrl} />;
+  const [pokemonName, setPokemonName] = useState<string>("");
+  const [pokemonImg, setPokemonImg] = useState<string>("");
+
+  useEffect(() => {
+    const getPokemonData = async () => {
+      const response = await fetch(pokemonUrl);
+      const data = await response.json();
+
+      setPokemonName(data.name);
+      setPokemonImg(data.sprites.front_default);
+    };
+    getPokemonData();
+  }, [pokemonUrl]);
+
+  return <PokemonCard pokemonName={pokemonName} pokemonImg={pokemonImg} />;
 };
 
 export default Pokemon;
