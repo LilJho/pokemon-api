6fba12ae600575932a98811ecb612a4f25eec8dc
LilJho <jhonnelg25@gmail.com>
2023-06-09 20:46:27 +0800

    added next button and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,41 @@ import Pokemon from "./pages/Pokemon";
 export interface PokemonType {
   name: string;
   url: string;
+  next: string;
 }
 
 function App() {
   const [pokemons, setPokemons] = useState<PokemonType[]>([]);
+  const [nextUrl, setNextUrl] = useState<string>("");
+
+  const getPokemonsList = async (
+    url: string = "https://pokeapi.co/api/v2/pokemon"
+  ) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+
+    setPokemons(data.results);
+    setNextUrl(data.next);
+  };
 
   useEffect(() => {
-    const getPokemonsList = async () => {
-      const response = await fetch("https://pokeapi.co/api/v2/pokemon");
-      const data = await response.json();
-
-      setPokemons(data.results);
-    };
-
     getPokemonsList();
   }, []);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<PokemonList pokemons={pokemons} />} />
+        <Route
+          index
+          element={
+            <PokemonList
+              pokemons={pokemons}
+              nextUrl={nextUrl}
+              getPokemonsList={getPokemonsList}
+            />
+          }
+        />
         <Route path={"/pokemon"} element={<Pokemon />} />
       </Routes>
     </BrowserRouter>
--- src/components/screen/PokemonList.tsx
+++ src/components/screen/PokemonList.tsx
@@ -4,27 +4,33 @@ import { Link, Outlet } from "react-router-dom";
 
 interface PropsType {
   pokemons: PokemonType[];
+  nextUrl: string;
+  getPokemonsList: (url: string) => void;
 }
 
-const PokemonList = ({ pokemons }: PropsType) => {
+const PokemonList = ({ pokemons, nextUrl, getPokemonsList }: PropsType) => {
   return (
-    <>
-      <div className="flex flex-col items-center justify-center w-screen h-screen">
-        <h1 className="font-bold text-red-500">
-          Top {pokemons.length} Pokemons
-        </h1>
-        <ul>
-          {pokemons.map((pokemon) => (
-            <li key={pokemon.name}>
-              <Link to={"/pokemon"} state={{ pokemonUrl: pokemon.url }}>
-                {pokemon.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <Outlet />
+    <div className="flex flex-col items-center justify-center w-screen h-screen">
+      <h1 className="font-bold text-red-500">Top {pokemons.length} Pokemons</h1>
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.name}>
+            <Link to={"/pokemon"} state={{ pokemonUrl: pokemon.url }}>
+              {pokemon.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <button
+          onClick={() => getPokemonsList(nextUrl)}
+          className="p-2 mt-10 font-bold text-white bg-blue-800 rounded-sm"
+        >
+          Next
+        </button>
       </div>
-    </>
+      <Outlet />
+    </div>
   );
 };
 
